82bae5cb0dfd2be9d74b08057ed3b2806568ad9a
Guiwang <guixiong97@sina.cn>
2018-04-16 15:26:16 +0800

    AdminESP clean

--- Admin/esp-admin/src/main/java/com/cnpc/admin/mapper/ServerManagerMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cnpc.admin.mapper;
-
-import com.cnpc.admin.entity.ServerManager;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ServerManagerMapper extends Mapper<ServerManager> {
-}
--- Admin/esp-common/src/main/java/com/cnpc/common/constant/UserConstant.java
+++
@@ -1,8 +0,0 @@
-package com.cnpc.common.constant;
-
-
-public class UserConstant {
-    public static int PW_ENCORDER_SALT = 12;
-
-    public static String USER_NOT_EXIST="-1";
-}
